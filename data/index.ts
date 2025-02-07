accce96c6bf185fceecb646d0c6ac78442d56132
thanosdrome <ranjanshudhanshu104@gmail.com>
2025-02-07 14:12:43 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,206 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Ciphers Security ",
+    description: "Enhancing user retention and content readability",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building WEB3 based WebApps",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "BRAINWAVE- LANDING PAGE",
+    des: "Brainwave - Modern UI/UX website, developed using React.js and Tailwind CSS, exemplifies modern UI/UX principles.",
+    img: "brainwave.png",
+    iconLists: [],
+    link: "https://thanosdrome.github.io/brainwave/",
+  },
+  {
+    id: 2,
+    title: "DCB INTERIOR DESIGNER",
+    des: "Designed and Created Web design which showcases a sleek and modern aesthetic, reflecting their expertise in creating sophisticated interior spaces. ",
+    img: "/dcbid.png",
+    iconLists: [],
+    link: "https://dcbid.co.in/",
+    
+  },
+  {
+    id: 3,
+    title: "Professional Academy",
+    des: "Designed and Developed a user-friendly, visually engaging, and highly functional website for a career guidance agency to help students and professionals make informed career decisions.",
+    img: "/proacademy.png",
+    iconLists: [],
+    link: "https://proacademyvadodara.com/",
+  },
+  {
+    id: 4,
+    title: "Amrit Tours and Travels",
+    des: "Landing Page for Trous and Travel Agency with Taxi Services",
+    img: "/amrittravels.png",
+    iconLists: [],
+    link: "https://amrittoursandtravels.com/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Shudhanshu completely transformed my website into a sleek, modern, and user-friendly platform. His attention to detail and understanding of my brand made all the difference. Thanks to his work, my online presence has never been stronger!",
+    title: "Director of Digital Expertz",
+  },
+  {
+    quote:
+      "Shudhanshu took my vision and turned it into a beautifully designed website that reflects my brand perfectly. The interface is intuitive, and my customers love the seamless experience. Highly recommend him to any small business owner!",
+    title: "Dhaval Barot - DBCID",
+  },
+  {
+    quote:
+      "Before working with Shudhanshu, my website was cluttered and hard to navigate. He redesigned it with a clean and engaging UI, making it super easy for customers to browse and purchase. My sales have increased significantly, and I couldn't be happier!",
+    title: "Director of OPS School Karnal",
+  },
+  {
+    quote:
+      "Shudhanshu not only created a visually stunning website for my business but also ensured that it worked flawlessly on all devices. His expertise in user experience has made my website so much more engaging and efficient. I highly recommend his services!",
+    title: "Arwaz Siddiqui - Interior Designer",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Web Developer (WORDPRESS) | Digital Expertz",
+    desc: "Designed intuitive and engaging product interfaces, increasing customer satisfaction scores by 20% Designed responsive, mobile-first websites from design concepts.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Full-Stack Developer Intern | IEEE Bombay Section & Alhansat solutions",
+    desc: "Improved user interfaces to elevate the overall user experience Contributed to dynamic web applications",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Web Development Intern | CodeClause",
+    desc: "Designed webpages on Figma and devloped website Worked on web projects, created web-apps using React.js",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Freelance UI/UX Designer",
+    desc: "Interacted with clients to identify business requirements, Worked with a proficient understanding of design tools",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/thanosdrome"
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/ranjanshu/"
+  },
+];
